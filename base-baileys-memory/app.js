8789dc8dc52ed357544dd4beef1904a8f84de039
Martin Galara <mgalara@gmail.com>
2023-03-20 16:38:42 -0300

    nodemailer y primeros flujos

--- base-baileys-memory/app.js
+++ base-baileys-memory/app.js
@@ -4,67 +4,110 @@ const QRPortalWeb = require('@bot-whatsapp/portal')
 const BaileysProvider = require('@bot-whatsapp/provider/baileys')
 const MockAdapter = require('@bot-whatsapp/database/mock')
 
-const flowSecundario = addKeyword(['2', 'siguiente']).addAnswer(['📄 Aquí tenemos el flujo secundario'])
+const axios = require('axios')
 
-const flowDocs = addKeyword(['doc', 'documentacion', 'documentación']).addAnswer(
-    [
-        '📄 Aquí encontras las documentación recuerda que puedes mejorarla',
-        'https://bot-whatsapp.netlify.app/',
-        '\n*2* Para siguiente paso.',
-    ],
-    null,
-    null,
-    [flowSecundario]
-)
+const nodemailer = require("nodemailer")
 
-const flowTuto = addKeyword(['tutorial', 'tuto']).addAnswer(
-    [
-        '🙌 Aquí encontras un ejemplo rapido',
-        'https://bot-whatsapp.netlify.app/docs/example/',
-        '\n*2* Para siguiente paso.',
-    ],
-    null,
-    null,
-    [flowSecundario]
-)
+//brjcksjmouxhgpdo
 
-const flowGracias = addKeyword(['gracias', 'grac']).addAnswer(
-    [
-        '🚀 Puedes aportar tu granito de arena a este proyecto',
-        '[*opencollective*] https://opencollective.com/bot-whatsapp',
-        '[*buymeacoffee*] https://www.buymeacoffee.com/leifermendez',
-        '[*patreon*] https://www.patreon.com/leifermendez',
-        '\n*2* Para siguiente paso.',
-    ],
-    null,
-    null,
-    [flowSecundario]
-)
+let ticket = {}
 
-const flowDiscord = addKeyword(['discord']).addAnswer(
-    ['🤪 Únete al discord', 'https://link.codigoencasa.com/DISCORD', '\n*2* Para siguiente paso.'],
-    null,
-    null,
-    [flowSecundario]
-)
+const sendEmail = async (ticket) => {
 
-const flowPrincipal = addKeyword(['hola', 'ole', 'alo'])
-    .addAnswer('🙌 Hola bienvenido a este *Chatbot*')
-    .addAnswer(
-        [
-            'te comparto los siguientes links de interes sobre el proyecto',
-            '👉 *doc* para ver la documentación',
-            '👉 *gracias*  para ver la lista de videos',
-            '👉 *discord* unirte al discord',
-        ],
-        null,
-        null,
-        [flowDocs, flowGracias, flowTuto, flowDiscord]
-    )
+    console.log(ticket)
+
+    let transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true, // true for 465, false for other ports
+        auth: {
+          user: "mgalara@gmail.com", // generated ethereal user
+          pass: "brjcksjmouxhgpdo", // generated ethereal password
+        },
+      });
+    
+      await transporter.sendMail({
+        from: '"Ticket" <mgalara@gmail.com>', // sender address
+        to: `mgalara2@gmail.com`, // list of receivers
+        subject: "Un cliente necesita soporte", // Subject line
+        text: "ASD123", // plain text body
+        html: `
+        <div>
+        <p>Datos del ticket</p>
+        <p>Correo del cliente: ${ticket.email}</p>
+        <p>Descripcion del problema: ${ticket.description}</p>
+        <p>Telefono de contacto: ${ticket.phone}</p>
+        <br></br>
+        <div/>
+        `, // html body
+      });
+
+}
+
+const validateUser = async (email) => {
+    
+    if(email === "mgalara@gmail.com")
+    {
+        return {
+            email: "mgalara@gmail.com",
+            hashPassword: "35284017"
+        }
+    }
+    else{
+        return false
+    }
+    const config = {
+        method: 'get',
+        url: `http://localhost:3001/users?email=${email}`,
+    }
+
+    const user = await axios(config)
+    if(user.data.lenth !== 0){
+        return user.data[0]
+    }
+    else{
+        return false
+    }
+}
+
+const flujoPrincipal = addKeyword(['hola','soporte','ayuda'])
+.addAnswer(['Gracias por comunicarte con Sistema Siges.','Para generar un ticket de soporte necesitamos validar la cuenta.','Por favor ingresa el correo electronico.'],
+{
+    capture: true
+},
+async (ctx, {fallBack,flowDynamic}) => {
+    if(!ctx.body.includes('@')) {
+        await flowDynamic([{body:'Correo invalido'}])
+        return fallBack()
+    }
+    let email = ctx.body
+    const user = validateUser(email)
+    if(!user){
+        await flowDynamic([{body:'Correo invalido'}])
+        return fallBack()
+    }
+    ticket.email = email
+})
+.addAnswer(['Ingresa una descripcion del problema'],
+{
+    capture: true
+},
+(ctx) => {
+    ticket.description = ctx.body
+})
+.addAnswer(['Ingresa un telefono de contacto'],
+{
+    capture: true
+},
+(ctx) => {
+    ticket.phone = ctx.body
+    sendEmail(ticket)
+})
+.addAnswer(['A la brevedad un operador se contactara con ustedes para solucionar el problema','Gracias por comunicarse!'])
 
 const main = async () => {
     const adapterDB = new MockAdapter()
-    const adapterFlow = createFlow([flowPrincipal])
+    const adapterFlow = createFlow([flujoPrincipal])
     const adapterProvider = createProvider(BaileysProvider)
 
     createBot({
